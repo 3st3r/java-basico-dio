cdcbc8ee9fc4074f6a54f6360ca777d7e91d25fc
Ester_Emanuele <esterDevjava@gmail.com>
2023-09-02 12:36:16 -0300

    adicionando novas variáveis com operadores unários

--- tipos-variaveis/src/SmartTv.java
+++ tipos-variaveis/src/SmartTv.java
@@ -1,7 +1,19 @@
   public class SmartTv {
+    
    boolean ligada = false;
    int canal = 1;
    int volume = 10;
+  
+    
+    public void aumentarVolume(){
+      //volume+1
+       volume++;
+    }
+    public void diminuirVolume(){
+      //volume-1
+       volume--;
+    }
+
 
     public void ligar(){
         ligada = true;
